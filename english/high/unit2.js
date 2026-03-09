d157f66f7c0ba00812c82f6cadbccff9786add8e
klenime7 <204958564+klenime7@users.noreply.github.com>
2026-03-09 23:30:45 +0800

    ver0.0

---
+++ english/high/unit2.js
@@ -0,0 +1,48 @@
+registerBook("Unit 2 Intermediate", {
+  icon: "📘",
+  desc: "More words and phrases for daily use",
+  badge: "Intermediate",
+  content: `
+able adj.有能力的
+disable v.使傷殘
+enable v.使能夠
+ability n.能力
+trip v.絆倒 n.旅遊
+allow v.允許;容許
+computer n.電腦
+final adj.最後的 n.決賽
+finally adv.最後;終於
+happen v.發生
+treat v.對待;款待;治療 n.請客
+treatment n.治療;對待
+serious adj.認真的;嚴重的
+serve v.供應;服務;提供
+service n.服務
+surprise v.使驚訝 n.驚訝
+surprised adj.感到驚訝的
+surprising adj.令人驚訝的
+fight v.打架;爭吵 n.爭論;爭吵
+notice v.注意 n.布告;通知;注意
+worry v.擔心 n.擔心
+worried adj.擔心的
+tip n.祕訣;小費
+act v.行動;扮演 n.行為
+action n.行動
+share v.分享 n.一份
+raise v.養育;舉起;提高 n.提高;加薪
+dress v.穿衣;打扮 n.洋裝;服裝
+traffic n.交通
+film v.拍電影 n.底片;電影
+drop v.掉落;放棄 n.下降
+heat v.加熱 n.熱度
+attack v.攻擊;抨擊 n.攻擊;抨擊
+item n.項目;一則
+hurt v.受傷;疼痛
+enter v.進入;輸入
+paint v.油漆;繪畫 n.油漆;顏料
+painting n.畫
+throat n.喉嚨
+meal n.一餐
+hide v.隱藏;躲藏
+`,
+});
---
+++ english/high/unit3.js
@@ -0,0 +1,60 @@
+registerBook("Unit 3 Intermediate", {
+  icon: "📗",
+  desc: "Words and phrases for more advanced learners",
+  badge: "Intermediate",
+  content: `
+  interest v.使感興趣 n.興趣;利息
+interested adj.感興趣的
+interesting adj.有趣的
+collect v.蒐集;募捐
+collection n.蒐集;募捐
+difficult adj.困難的;難相處的
+difficulty n.困難
+reason n.理由;原因
+reasonable adj.合理的;通情達理的
+expect v.預期;期盼;要求
+unexpectedly adv.出乎意料地
+foreign adj.外國的
+foreigner n.外國人
+celebrate v.慶祝
+possible adj.可能的
+possibility n.可能性
+language n.語言
+light n.光;光線;光亮 v.點燃 adj.明亮的;輕的;淺色的
+lighten v.減輕;照亮
+medicine n.醫學;藥
+medical adj.醫療的
+knowledge n.知識;知曉
+dead adj.死的;無效的 n.死者
+deadly adj.致命的 adv.極度地
+death n.死亡
+fail v.失敗;不及格
+failure n.失敗
+near adj.近的 prep.在附近
+nearby adv.在附近
+danger n.危險
+dangerous adj.危險的
+package n.包裹 v.打包;包裝
+nature n.天性;自然
+natural adj.天然的;天生的
+naturally adv.自然地;天生地
+plant v.種植 n.植物;工廠
+feed v.餵養;以此為食
+hit v.碰撞;打擊;侵襲 n.成功而風行一時的事物
+terrible adj.可怕的;糟的
+terribly adv.非常地
+terrific adj.非常好的
+pleasure n.愉快;樂趣;榮幸
+pleased adj.高興的
+pleasant adj.令人愉快的
+practice v.練習;實行;執業 n.練習;實行;執業
+practical adj.實用的;實際的
+robot n.機器人
+sore adj.疼痛的
+insect n.昆蟲
+safe adj.安全的 n.保險箱
+safety n.安全
+tie n.關係;聯繫;領帶 v.繫;綑
+bear v.承受;忍受;生(小孩) n.熊
+`,
+});
